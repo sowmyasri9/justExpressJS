780175615d17b76b900321e2cee474e83f387f81
sowmyasri9 <sowmyasri23164@gmail.com>
2021-08-31 01:21:01 -0700

    Used middlewares and view engine ejs

---
+++ express201/appUse.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+function validateUser(req,res,next){
+    res.locals.validated=true;
+    console.log("Validated ran!");
+    next()
+}
+app.use(validateUser);
+
+app.use('/admin',validateUser)
+
+app.get('/',validateUser);
+app.get('/',(req,res,next)=>{
+res.send("<h1>Main Page</h1>")
+console.log(res.locals.validated);
+})
+
+app.get('/admin',(req,res,next)=>{
+    res.send("<h1>Admin Page</h1>")
+    console.log(res.locals.validated);
+    })
+
+app.listen(3000);
